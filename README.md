cbc7f22f03c82d8134c826873d9b39578109594e
Marcia Agostinho <marciaagostinho27@gmail.com>
2022-07-16 11:00:21 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,23 @@
+#### Desafio Lousa Mágica
+
+#### Projeto Criar uma lousa mágica para diversão das crianças.
+
+##### Na lousa tem a opção de desenhar, mudar as cores da caneta, apagar ou baixar o desenho.
+
+
+[<img src="img/tela-lousa.gif">]
+
+
+### Tecnologias utilizadas
+
+<div flex-direction: row>
+
+ <img align="center" alt="Js" height="30" width="40" src="https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-plain.svg">
+
+  <img align="center" alt="HTML" height="30" width="40" src="https://raw.githubusercontent.com/devicons/devicon/master/icons/html5/html5-original.svg">
+  <img align="center" alt="CSS" height="30" width="40" src="https://raw.githubusercontent.com/devicons/devicon/master/icons/css3/css3-original.svg">
+
+  <br>
+  <br>
+
+[<img  height="150" width="150" src="img/marcia.jpeg"> ]
---
+++ js/index.js
@@ -0,0 +1,103 @@
+
+let canvas = document.querySelector('main .drawingBoard');
+let ctxCanvas = canvas.getContext('2d');
+let currentColor = 'black';
+let canDraw = false;
+let mouseX = 0;
+let mouseY = 0;
+let btnEraser = document.querySelector('.buttons .btnEraser');
+let btnDownload = document.querySelector('.buttons .btnDownload');
+
+
+btnEraser.addEventListener('click', switchCursor);
+btnDownload.addEventListener('click', downloadDraw);
+
+document.querySelectorAll('.colorPalette .color').forEach((color) => {
+    color.addEventListener('click', selectColor);
+})
+
+
+canvas.addEventListener('mousedown', mouseDownEvent);
+canvas.addEventListener('mousemove', mouseMoveEvent);
+canvas.addEventListener('mouseup', mouseUpEvent);
+canvas.addEventListener('touchatart', touchStart)
+
+document.querySelector('.buttons .btnCleanAll').addEventListener('click', cleanDraw);
+
+// FUNCTIONS
+
+// Transformando cursor normal para borracha
+function switchCursor(e) {
+    let cursor = e.target.getAttribute('data-cursor');
+    
+    if ( cursor == 'eraser' ) {
+        e.target.src = '../assets/images/crosshair.png';
+        e.target.setAttribute('data-cursor', 'crosshair');
+        
+    } else {
+        e.target.src = '../assets/images/eraser.png';
+        e.target.setAttribute('data-cursor', 'eraser')
+    }
+}
+
+// Função que faz download do desenho
+function downloadDraw() {
+    btnDownload.download = 'Desenho-canvas.png';
+    btnDownload.href = canvas.toDataURL('image/png');
+}
+
+// Selecionando a cor do pincel
+function selectColor(e) {
+    currentColor = e.target.getAttribute('data-color')
+    
+    document.querySelector('.colorPalette .color.active').classList.remove('active');
+    e.target.classList.add('active');
+}
+
+// Funções de clique na tela de desenho
+function mouseDownEvent(e) {
+    canDraw = true;
+    mouseX = e.pageX - canvas.offsetLeft;
+    mouseY = e.pageY - canvas.offsetTop;
+}  
+
+function mouseMoveEvent(e) {
+    if( canDraw ) {
+        draw(e.pageX, e.pageY);
+    }
+}
+
+function  touchStart(e) {
+    if( canDraw ) {
+        draw(e.pageX, e.pageY);
+    }
+}
+
+function mouseUpEvent() {
+    canDraw = false;
+}
+
+
+// Função que desenha efetivamente
+function draw(x, y) {
+    let pointX = x - canvas.offsetLeft;
+    let pointY = y - canvas.offsetTop;
+
+    ctxCanvas.beginPath();
+    ctxCanvas.lineWidth = 3;
+    ctxCanvas.lineJoin = 'round';
+    ctxCanvas.moveTo(mouseX, mouseY);
+    ctxCanvas.lineTo(pointX, pointY);
+    ctxCanvas.closePath();
+    ctxCanvas.strokeStyle = currentColor;
+    ctxCanvas.stroke();
+
+    mouseX = pointX;
+    mouseY = pointY;
+}
+
+// Função que limpa o quadro
+function cleanDraw() {
+    ctxCanvas.setTransform(1, 0, 0, 1, 0, 0);
+    ctxCanvas.clearRect(0, 0, ctxCanvas.canvas.width, ctxCanvas.canvas.height);
+}
